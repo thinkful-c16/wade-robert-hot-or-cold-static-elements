44df443353f19e61ca92fdfcdf98939f5a26039e
Robert Frazier <robert.v.frazier@gmail.com>
2017-12-11 17:18:59 -0500

    Plus

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ export default function Header() {
             <a className="what" href="#">What ?</a>
           </li>
           <li>
-            <a className="new" href="#">New Game</a>
+            <a className="new" href="#">+New Game</a>
           </li>
         </ul>
       </nav>
